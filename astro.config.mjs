dc73ab22cf4d53a0d03b9116ca705705242a8423
Fran <fducekuce@gmail.com>
2024-09-03 21:57:56 +0200

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config"
 import partytown from "@astrojs/partytown"
 import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
+import vercel from "@astrojs/vercel/serverless"
 
 import sitemap from "@astrojs/sitemap"
 
@@ -18,4 +19,8 @@ export default defineConfig({
       },
     }),
   ],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 })
