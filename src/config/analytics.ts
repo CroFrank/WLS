831716a70b49dcd06c30876cb6d1c8d85a7f89f9
Fran <fducekuce@gmail.com>
2024-09-12 15:39:18 +0200

    remove google analytics

--- src/config/analytics.ts
+++
@@ -1,8 +0,0 @@
-// Google tracking
-// - Google Site Verification: for Google Search Console
-// - Google Analytics Measurement ID: for Google Analytics
-// - Google Tag Manager ID: for Google Tag Manager
-
-export const googleSiteVerification = ''
-export const googleAnalyticsMeasurementID = 'G-6JVV6NQT7N'
-export const googleTagManagerID = ''
