0f176663532be8c50c7a176375639da2b0f91b5c
Fran <fducekuce@gmail.com>
2025-02-24 10:00:52 +0100

    improve contact form

---
+++ src/pages/api/email.ts
@@ -0,0 +1,34 @@
+import type { APIRoute } from 'astro'
+
+const formSpar = import.meta.env.FORMSPARK
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+	const formData = await request.formData()
+	const email = formData.get('email')?.toString()
+	const checkbox = formData.get('checkbox')?.toString()
+	const lovac = formData.get('lovac')?.toString()
+	if (request.method !== 'POST') {
+		console.log('Method Not Allowed')
+		return redirect('/kontakt')
+	}
+	if (!email || checkbox !== 'on') {
+		console.log('Nisu popunjena obavezna polja')
+		return redirect('/kontakt')
+	}
+	if (lovac) {
+		console.log('honeypot spam detected')
+		return redirect('/kontakt')
+	}
+
+	const response = await fetch(`https://submit-form.com/${formSpar}`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(Object.fromEntries(formData))
+	})
+
+	if (!response.ok) {
+		console.log('response error')
+		return redirect('/kontakt')
+	}
+	return redirect(`/success-page?email=${email}`)
+}
